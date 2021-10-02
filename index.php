411373a537de0653103a408ba857c9019c036a42
thierry bibal <thierry.prof29@orange.fr>
2021-10-02 16:23:45 +0200

    index initial

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+          <meta charset="utf-8" />
+          <title> base de donnees </title>
+    </head>
+    <body style="background-color:#1abc9c">
+
+    <h1> GESTION DE LA BASE DE DONNEE "nomenclature" </h1>
+        <form action="formulaire_1.php" method="post">
+            <input type="submit" value="rechercher_1_code" />
+        </form>
+        <form action="formulaire_2.php" method="post">
+            <input type="submit" value="rechercher_2_codes" />
+        </form>
+        <form action="formulaire_3.php" method="post">
+            <input type="submit" value="rechercher_3_codes" />
+        </form>
+        <form action="formulaire_4.php" method="post">
+            <input type="submit" value="rechercher_4_codes" />
+        </form>
+
+        <p> </p>
+
+
+    </body>
+
+</html>
