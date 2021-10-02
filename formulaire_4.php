3b274a61101bc40510408346d97fd2473725aec8
thierry bibal <thierry.prof29@orange.fr>
2021-10-02 16:29:38 +0200

    formulaire initial

---
+++ formulaire_4.php
@@ -0,0 +1,42 @@
+
+<p>
+    Bonjour <br />
+    Saisi les codes et les quantités:
+</p>
+
+<form action="resultat.php" method="POST">
+
+<p>
+ Saisir le code 1:
+ <input type="text" name="code_par_1" /> <br />
+ Saisir une quantité 1:
+ <input type="text" name="qte_1" /> <br />
+
+ Saisir le code 2:
+ <input type="text" name="code_par_2" /> <br />
+ Saisir une quantité 2:
+ <input type="text" name="qte_2" /> <br />
+
+ Saisir le code 3:
+ <input type="text" name="code_par_3" /> <br />
+ Saisir une quantité 3:
+ <input type="text" name="qte_3" /> <br />
+
+ Saisir le code 4:
+ <input type="text" name="code_par_4" /> <br />
+ Saisir une quantité 4:
+ <input type="text" name="qte_4" /> <br />
+
+ Saisir le code F de l'affaire:
+ <input type="text" name="code_F" /> <br />
+
+<input type="submit" value="valider" />
+</p>
+
+ </form>
+
+ <form action="index.php" method="post">
+    <p>
+    <input type="submit" value="retour" />
+    </p>
+    </form>
