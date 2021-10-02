be2af16903f42c155283401ce971c46d8b0c02fc
thierry bibal <thierry.prof29@orange.fr>
2021-10-02 16:28:15 +0200

    programme initial

---
+++ formulaire_2.php
@@ -0,0 +1,33 @@
+
+<p>
+    Bonjour <br />
+    Saisi les codes et les quantités:
+</p>
+
+<form action="resultat.php" method="POST">
+
+<p>
+ Saisir le code 1:
+ <input type="text" name="code_par_1" /> <br />
+ Saisir une quantité 1:
+ <input type="text" name="qte_1" /> <br />
+
+ Saisir le code 2:
+ <input type="text" name="code_par_2" /> <br />
+ Saisir une quantité 2:
+ <input type="text" name="qte_2" /> <br />
+
+ Saisir le code F de l'affaire:
+ <input type="text" name="code_F" /> <br />
+
+
+<input type="submit" value="valider" />
+</p>
+
+ </form>
+ <form action="index.php" method="post">
+    <p>
+    <input type="submit" value="retour" />
+    </p>
+    </form>
+
